bb0be5f0f5f49ef31d32a7a450362142dc28d20c
phamtanphat <phatdroid94@gmail.com>
2022-08-12 19:15:12 +0700

    api service

--- lib/common/app_constant.dart
+++ lib/common/app_constant.dart
@@ -2,4 +2,5 @@ class AppConstant {
   static const routeHomePage = '/home_page';
 
   static const baseUrl = 'https://api.openweathermap.org/';
+  static const appId = "2206d84c8189efe365465e3318f487aa";
 }
