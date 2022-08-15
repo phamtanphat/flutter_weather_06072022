1f09a1189cd2a6af22b5cb71482074bc25371e92
phamtanphat <phatdroid94@gmail.com>
2022-08-15 18:07:24 +0700

    test notifilistener

--- lib/presentations/features/home/home_controller.dart
+++ lib/presentations/features/home/home_controller.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_weather_06072022/data/repository/climate_repository.dart';
 
 class HomeController extends ChangeNotifier {
   late ClimateRepository _climateRepository;
+  String data = "";
 
   void updateClimateRepository({required ClimateRepository climateRepository}) {
     _climateRepository = climateRepository;
@@ -10,8 +13,11 @@ class HomeController extends ChangeNotifier {
 
   void getTempFromCityName({String cityName = ""}) async{
     try {
+
       var response = await _climateRepository.getTempFromCityName(cityName: cityName);
-      print(response.toString());
+      data = Random().nextInt(100).toString();
+      notifyListeners();
+
     }catch(e) {
       print(e.toString());
     }
--- lib/presentations/features/home/home_page.dart
+++ lib/presentations/features/home/home_page.dart
@@ -3,30 +3,73 @@ import 'package:flutter_weather_06072022/data/remote/api/api_service.dart';
 import 'package:flutter_weather_06072022/data/repository/climate_repository.dart';
 import 'package:flutter_weather_06072022/presentations/features/home/home_controller.dart';
 import 'package:provider/provider.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          Provider(create: (context) => ApiService()),
-          ProxyProvider<ApiService, ClimateRepository>(
-              update: (context, apiService, climateRepository) {
-                climateRepository ??= ClimateRepository();
-                climateRepository.updateClimateRepository(apiService: apiService);
-                return climateRepository;
-              }
-          ),
-          ChangeNotifierProxyProvider<ClimateRepository, HomeController>(
-              create: (context) => HomeController(),
-              update: (context, repository, controller) {
-                controller ??= HomeController();
-                controller.updateClimateRepository(climateRepository: repository);
-                return controller;
-              }
-          )
-        ]
+      providers: [
+        Provider(create: (context) => ApiService()),
+        ProxyProvider<ApiService, ClimateRepository>(
+            update: (context, apiService, climateRepository) {
+          climateRepository ??= ClimateRepository();
+          climateRepository.updateClimateRepository(apiService: apiService);
+          return climateRepository;
+        }),
+        ChangeNotifierProxyProvider<ClimateRepository, HomeController>(
+            create: (context) => HomeController(),
+            update: (context, repository, controller) {
+              controller ??= HomeController();
+              controller.updateClimateRepository(climateRepository: repository);
+              return controller;
+            })
+      ],
+      child: HomeDemo(),
+    );
+  }
+}
+
+class HomeDemo extends StatefulWidget {
+  const HomeDemo({Key? key}) : super(key: key);
+
+  @override
+  State<HomeDemo> createState() => _HomeDemoState();
+}
+
+class _HomeDemoState extends State<HomeDemo> {
+  late HomeController homeController;
+
+  @override
+  void initState() {
+    super.initState();
+    homeController = context.read();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeDemo oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    homeController.getTempFromCityName(cityName: "Hanoi");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Build");
+    return Scaffold(
+      body: Consumer<HomeController>(
+          builder: (context, controller, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {
+                  homeController.getTempFromCityName(cityName: "Hanoi");
+                }, child: Text("Change")),
+                Center(child: Text(controller.data)),
+              ],
+            );
+          }
+      ),
     );
   }
 }
